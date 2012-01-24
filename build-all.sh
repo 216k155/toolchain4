c0cd58dcdb42a4cbce64cba4b9e9e4904c6b1821
Ray Donnelly <mingw.android@gmail.com>
2012-01-24 22:13:55 +0000

    Added ar-printf-newer.diff
    Something's up with ar-printf.diff
    
    sometimes it doesn't apply
    
    seems host machine dependant

--- build-all.sh
+++ build-all.sh
@@ -9,7 +9,7 @@ CCTOOLSVER=782 FOREIGNHEADERS=1 ./toolchain.sh $1
 CCTOOLSVER=809 FOREIGNHEADERS=0 ./toolchain.sh $1
 CCTOOLSVER=809 FOREIGNHEADERS=1 ./toolchain.sh $1
 
-[[ -f tc4-bld-src-$(uname-bt).7z ]] && rm rc-bld-src-$(uname-bt).7z£
+[[ -f tc4-bld-src-$(uname-bt).7z ]] && rm rc-bld-src-$(uname-bt).7z
 
 mv src src-$(uname-bt)
 mv bld bld-$(uname-bt)
