aa4db5231fd58060a878c3a1ee2608631a07bc69
Ray Donnelly <mingw.android@laptop.com>
2012-04-07 14:20:03 +0100

    better testing framework

--- test-darwin-compilers.sh
+++ test-darwin-compilers.sh
@@ -6,6 +6,8 @@
 
 if [[ "$(uname-bt)" = "Linux" ]] ; then
 	STRACE="strace -F -s 512 -e access,open,execve,stat64,fstat64"
+elif [[ "$(uname-bt)" = "Darwin" ]] ; then
+	STRACE="dtruss"
 fi
 
 download http://dl.google.com/android/ndk/android-ndk-r6b-linux-x86.tar.bz2
@@ -43,11 +45,11 @@ fi
 # MOVED_TOOLCHAIN=/tmp2/$RIGHT/bin/i686-apple-darwin11
 # ARCHS="-m64"
 
-FIXED_TOOLCHAIN=/tmp2/$LEFT/bin/${LEFT}-
+FIXED_TOOLCHAIN=/tmp2/$LEFT/bin/${LEFT}-g++
 if [[ "$RIGHT" = "/" ]] ; then
-    MOVED_TOOLCHAIN=/usr/bin/
+	MOVED_TOOLCHAIN=/usr/bin/llvm-g++-4.2
 else
-    MOVED_TOOLCHAIN=/tmp2/$RIGHT/bin/${RIGHT}-
+	MOVED_TOOLCHAIN=/tmp2/$RIGHT/bin/${RIGHT}-g++
 fi
 ARCHS="-arch x86_64 -arch i386"
 
@@ -55,18 +57,24 @@ SDK=$PWD/sdks/MacOSX10.7.sdk
 
 pushd android-ndk-r6b/sources/host-tools/ndk-stack
 
-${STRACE} ${FIXED_TOOLCHAIN}g++ $ARCHS -lstdc++ ndk-stack.c ndk-stack-parser.c elff/dwarf_cu.cc elff/dwarf_die.cc elff/dwarf_utils.cc elff/elf_alloc.cc elff/elf_file.cc elff/elf_mapped_section.cc elff/elff_api.cc elff/mapfile.c regex/regcomp.c regex/regerror.c regex/regexec.c regex/regfree.c -o $OUTLEFT/ndk-stack --sysroot $SDK > $OUTLEFT/strace.txt 2>&1
-${STRACE} ${MOVED_TOOLCHAIN}g++ $ARCHS -lstdc++ ndk-stack.c ndk-stack-parser.c elff/dwarf_cu.cc elff/dwarf_die.cc elff/dwarf_utils.cc elff/elf_alloc.cc elff/elf_file.cc elff/elf_mapped_section.cc elff/elff_api.cc elff/mapfile.c regex/regcomp.c regex/regerror.c regex/regexec.c regex/regfree.c -o $OUTRIGHT/ndk-stack --sysroot $SDK > $OUTRIGHT/strace.txt 2>&1
+#dtruss -n $(basename ${FIXED_TOOLCHAIN}g++) > $OUTLEFT/dtrace.txt 2>&1 &
+${STRACE} ${FIXED_TOOLCHAIN} $ARCHS -lstdc++ ndk-stack.c ndk-stack-parser.c elff/dwarf_cu.cc elff/dwarf_die.cc elff/dwarf_utils.cc elff/elf_alloc.cc elff/elf_file.cc elff/elf_mapped_section.cc elff/elff_api.cc elff/mapfile.c regex/regcomp.c regex/regerror.c regex/regexec.c regex/regfree.c -o $OUTLEFT/ndk-stack --sysroot $SDK > $OUTLEFT/strace.txt 2>&1
+#dtruss -n $(basename ${MOVED_TOOLCHAIN}g++) > $OUTRIGHT/dtrace.txt 2>&1 &
+${STRACE} ${MOVED_TOOLCHAIN} $ARCHS -lstdc++ ndk-stack.c ndk-stack-parser.c elff/dwarf_cu.cc elff/dwarf_die.cc elff/dwarf_utils.cc elff/elf_alloc.cc elff/elf_file.cc elff/elf_mapped_section.cc elff/elff_api.cc elff/mapfile.c regex/regcomp.c regex/regerror.c regex/regexec.c regex/regfree.c -o $OUTRIGHT/ndk-stack --sysroot $SDK > $OUTRIGHT/strace.txt 2>&1
 
-${FIXED_TOOLCHAIN}g++ --print-search-dirs > $OUTLEFT/search-dirs.txt 2>&1
-${MOVED_TOOLCHAIN}g++ --print-search-dirs > $OUTRIGHT/search-dirs.txt 2>&1
-${FIXED_TOOLCHAIN}g++ -dumpspecs > $OUTLEFT/specs.txt 2>&1
-${MOVED_TOOLCHAIN}g++ -dumpspecs > $OUTRIGHT/specs.txt 2>&1
+${FIXED_TOOLCHAIN} --print-search-dirs > $OUTLEFT/search-dirs.txt 2>&1
+${MOVED_TOOLCHAIN} --print-search-dirs > $OUTRIGHT/search-dirs.txt 2>&1
+${FIXED_TOOLCHAIN} -dumpspecs > $OUTLEFT/specs.txt 2>&1
+${MOVED_TOOLCHAIN} -dumpspecs > $OUTRIGHT/specs.txt 2>&1
 
-${STRACE} ${FIXED_TOOLCHAIN}g++ -print-prog-name=as > $OUTLEFT/print-prog-name-as.txt 2>&1
-${STRACE} ${MOVED_TOOLCHAIN}g++ -print-prog-name=as > $OUTRIGHT/print-prog-name-as.txt 2>&1
+${STRACE} ${FIXED_TOOLCHAIN} -print-prog-name=as > $OUTLEFT/print-prog-name-as.txt 2>&1
+${STRACE} ${MOVED_TOOLCHAIN} -print-prog-name=as > $OUTRIGHT/print-prog-name-as.txt 2>&1
 
 popd
+
+rm -f $(uname-bt)-test.7z
+7za a -mx=9 $(uname-bt)-test.7z $OUTLEFT $OUTRIGHT
+cp $(uname-bt)-test.7z ~/Dropbox/darwin-compilers-work/
 
 if [[ $(which bcmp) ]] ; then
     bcmp $OUTLEFT/strace.txt $OUTRIGHT/strace.txt &
