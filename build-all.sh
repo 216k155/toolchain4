a4c6671f0dae38d6c04828cf2f9446146dae01d9
Ray Donnelly <mingw.android@gmail.com>
2012-04-10 00:56:35 +0100

    Separate builds into PREFIX-ios and PREFIX-osx folders

--- build-all.sh
+++ build-all.sh
@@ -15,8 +15,13 @@ fi
 #TARGET_ARCH=$2
 
 full_build_for_arch() {
-    local _PREFIX_SUFFIX=$1
     local _TARGET_ARCH=$2
+    if [[ "$_TARGET_ARCH" = "arm" ]] ; then
+        local _PREFIX_SUFFIX=$1-ios
+    else
+        local _PREFIX_SUFFIX=$1-osx
+    fi
+    rm -rf bld-$_PREFIX_SUFFIX src-$PREFIX_SUFFIX /tmp2/$PREFIX_SUFFIX
     PREFIX_SUFFIX=$_PREFIX_SUFFIX ./toolchain.sh llvmgcc-core $_TARGET_ARCH
     rm -rf bld-$1/cctools-809-${_TARGET_ARCH} src-$_PREFIX_SUFFIX/cctools-809
     PREFIX_SUFFIX=$_PREFIX_SUFFIX ./toolchain.sh cctools $_TARGET_ARCH
@@ -28,41 +33,42 @@ full_build_for_arch() {
 }
 
 # Clean everything.
-rm -rf bld-$PREFIX src-$PREFIX /tmp2/$PREFIX
 
 # Make arm build.
 full_build_for_arch $PREFIX arm
 
 # Remove Apple's proprietary stuff, backing up the list of what's needed.
-find /tmp2/$PREFIX/usr/lib > /tmp2/$PREFIX/arm-needed-libs.txt
-find /tmp2/$PREFIX/arm-apple-darwin11/lib >> /tmp2/$PREFIX/arm-needed-libs.txt
-rm -rf /tmp2/$PREFIX/usr/lib
-rm -rf /tmp2/$PREFIX/arm-apple-darwin11/lib
-find /tmp2/$PREFIX/usr/include > /tmp2/$PREFIX/arm-needed-headers.txt
-find /tmp2/$PREFIX/arm-apple-darwin11/sys-include >> /tmp2/$PREFIX/arm-needed-headers.txt
-rm -rf /tmp2/$PREFIX/usr/include
-rm -rf /tmp2/$PREFIX/arm-apple-darwin11/sys-include
+find /tmp2/${PREFIX}-ios/usr/lib > /tmp2/${PREFIX}-ios/needed-libs.txt
+find /tmp2/${PREFIX}-ios/arm-apple-darwin11/lib >> /tmp2/${PREFIX}-ios/needed-libs.txt
+rm -rf /tmp2/${PREFIX}-ios/usr/lib
+rm -rf /tmp2/${PREFIX}-ios/arm-apple-darwin11/lib
+find /tmp2/${PREFIX}-ios/usr/include > /tmp2/${PREFIX}-ios/needed-headers.txt
+find /tmp2/${PREFIX}-ios/arm-apple-darwin11/sys-include >> /tmp2/${PREFIX}-ios/needed-headers.txt
+rm -rf /tmp2/${PREFIX}-ios/usr/include
+rm -rf /tmp2/${PREFIX}-ios/arm-apple-darwin11/sys-include
 
 # Make i686 build.
 full_build_for_arch $PREFIX intel
 
 # Remove Apple's proprietary stuff, backing up the list of what's needed.
-find /tmp2/$PREFIX/usr/lib > /tmp2/$PREFIX/i686-needed-libs.txt
-find /tmp2/$PREFIX/i686-apple-darwin11/lib >> /tmp2/$PREFIX/i686-needed-libs.txt
-rm -rf /tmp2/$PREFIX/usr/lib
-rm -rf /tmp2/$PREFIX/i686-apple-darwin11/lib
-find /tmp2/$PREFIX/usr/include > /tmp2/$PREFIX/i686-needed-headers.txt
-find /tmp2/$PREFIX/i686-apple-darwin11/sys-include >> /tmp2/$PREFIX/i686-needed-headers.txt
-rm -rf /tmp2/$PREFIX/usr/include
-rm -rf /tmp2/$PREFIX/i686-apple-darwin11/sys-include
+find /tmp2/${PREFIX}-osx/usr/lib > /tmp2/${PREFIX}-osx/needed-libs.txt
+find /tmp2/${PREFIX}-osx/i686-apple-darwin11/lib >> /tmp2/${PREFIX}-osx/needed-libs.txt
+rm -rf /tmp2/${PREFIX}-osx/usr/lib
+rm -rf /tmp2/${PREFIX}-osx/i686-apple-darwin11/lib
+find /tmp2/${PREFIX}-osx/usr/include > /tmp2/${PREFIX}-osx/needed-headers.txt
+find /tmp2/${PREFIX}-osx/i686-apple-darwin11/sys-include >> /tmp2/${PREFIX}-osx/needed-headers.txt
+rm -rf /tmp2/${PREFIX}-osx/usr/include
+rm -rf /tmp2/${PREFIX}-osx/i686-apple-darwin11/sys-include
 
 # Strip executables.
-find /tmp2/$PREFIX/bin -type f -and -not \( -path "*-config" \) | xargs strip
-find /tmp2/$PREFIX/libexec -type f -and -not \( -path "*.sh" -or -path "*mkheaders" \) | xargs strip
+find /tmp2/${PREFIX}-ios/bin -type f -and -not \( -path "*-config" \) | xargs strip
+find /tmp2/${PREFIX}-ios/libexec -type f -and -not \( -path "*.sh" -or -path "*mkheaders" \) | xargs strip
+find /tmp2/${PREFIX}-osx/bin -type f -and -not \( -path "*-config" \) | xargs strip
+find /tmp2/${PREFIX}-osx/libexec -type f -and -not \( -path "*.sh" -or -path "*mkheaders" \) | xargs strip
 
-pushd /tmp2/$PREFIX
+pushd /tmp2
     UNAME=$(uname-bt)
-    7za a -mx=9 multiarch-darwin11-cctools127.2-gcc42-5666.3-llvmgcc42-2336.1-$UNAME.7z *
+    7za a -mx=9 multiarch-darwin11-cctools127.2-gcc42-5666.3-llvmgcc42-2336.1-$UNAME.7z ${PREFIX}-ios ${PREFIX}-osx
     cp multiarch-darwin11-cctools127.2-gcc42-5666.3-llvmgcc42-2336.1-$UNAME.7z ~/Dropbox/darwin-compilers-work
 popd
 
